13d44b146d3e1d60b7c54437d186ad6ae8e80dd2
yasmindatascience <reisamorinn@gmail.com>
2026-04-27 19:45:07 -0300

    Update queries.sql

--- analytics/queries.sql
+++ analytics/queries.sql
@@ -1,1 +1,11 @@
+-- faturamento total
+SELECT SUM(total) AS faturamento FROM orders;
 
+-- clientes que mais gastam
+SELECT customer_id, SUM(total) AS total_gasto
+FROM orders
+GROUP BY customer_id
+ORDER BY total_gasto DESC;
+
+-- quantidade de clientes
+SELECT COUNT(*) FROM customers;
